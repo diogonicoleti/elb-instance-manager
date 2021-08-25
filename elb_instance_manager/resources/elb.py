1ad238d2d049f305504e1592d0fc84e50df8b74a
Diogo Nicoleti <diogo.nicoleti@nubank.com.br>
2021-08-25 00:10:41 -0300

    Small improvements

--- elb_instance_manager/resources/elb.py
+++ elb_instance_manager/resources/elb.py
@@ -1,3 +1,4 @@
+import logging
 from botocore.exceptions import ClientError
 from flask_restful import Resource
 from flask import request
@@ -15,32 +16,49 @@ class ELBResource(Resource):
     def get(self, elb_name):
         try:
             alb_instance_ids = self.alb.get_instance_ids(elb_name)
-            instances = self.ec2.get_instances(alb_instance_ids) if alb_instance_ids else []
+            instances = self.ec2.get_instances(
+                alb_instance_ids) if alb_instance_ids else []
             return MachineInfoSchema(many=True).dump(instances), 200
         except ClientError as err:
-            if err.response['Error']['Code'] == 'LoadBalancerNotFound':
-                return 'Load Balancer not found', 404
-            else:
-                raise err
+            return self.__handle_client_error(err)
 
     def post(self, elb_name):
         try:
             id = self.__get_instance_id()
+            if id in self.alb.get_instance_ids(elb_name):
+                return 'Instance already on load balancer', 409
+
             self.alb.register(elb_name, id)
             return self.__get_instance(id), 201
         except ValidationError:
             return 'Wrong data format', 400
+        except ClientError as err:
+            return self.__handle_client_error(err)
 
     def delete(self, elb_name):
         try:
             id = self.__get_instance_id()
+            if id not in self.alb.get_instance_ids(elb_name):
+                return 'Instance is not on load balancer', 409
+
             self.alb.deregister(elb_name, id)
             return self.__get_instance(id), 201
         except ValidationError:
             return 'Wrong data format', 400
+        except ClientError as err:
+            return self.__handle_client_error(err)
 
     def __get_instance(self, instance_id):
         return MachineInfoSchema().dump(self.ec2.get_instance(instance_id))
 
     def __get_instance_id(self):
         return MachineIdSchema().load(request.get_json())
+
+    def __handle_client_error(self, err):
+        if err.response['Error']['Code'] == 'LoadBalancerNotFound':
+            return 'Load Balancer not found', 404
+        if err.response['Error']['Code'] == 'ValidationError':
+            return err.response['Error']['Message'], 400
+
+        logging.error('Unexpected error: %s' % err)
+        return 'Unexpected error', 500
