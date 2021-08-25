a33730d87970a19dc654dc2036fe339cae2cefa2
Diogo Nicoleti <diogo.nicoleti@nubank.com.br>
2021-08-24 21:56:54 -0300

    Format code

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 from setuptools import setup, find_packages
 
-
 with open('README.rst') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
-setup(
-    name='elb-instance-manager',
-    version='0.1.0',
-    description='A simple HTTP service to manage instances in a ELB',
-    long_description=readme,
-    author='Diogo Nicoleti',
-    author_email='diogo.nicoleti@gmail.com',
-    url='https://github.com/diogonicoleti/elb-instance-manager',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+setup(name='elb-instance-manager',
+      version='0.1.0',
+      description='A simple HTTP service to manage instances in a ELB',
+      long_description=readme,
+      author='Diogo Nicoleti',
+      author_email='diogo.nicoleti@gmail.com',
+      url='https://github.com/diogonicoleti/elb-instance-manager',
+      license=license,
+      packages=find_packages(exclude=('tests', 'docs')))
