3221818529138cd2ac22627e283509699c35c065
Diogo Nicoleti <diogo.nicoleti@nubank.com.br>
2021-08-25 00:11:23 -0300

    Code cleanup

--- elb_instance_manager/resources/health_check.py
+++ elb_instance_manager/resources/health_check.py
@@ -3,5 +3,4 @@ from flask_restful import Resource
 
 class HealthCheckResource(Resource):
     def get(self):
-        "Returns OK when the app is up and running"
         return {'status': 'OK'}, 200
--- elb_instance_manager/schemas/machine.py
+++ elb_instance_manager/schemas/machine.py
@@ -2,16 +2,16 @@ from marshmallow import Schema, fields, post_load
 
 
 class MachineInfoSchema(Schema):
-    InstanceId = fields.Str(required=True, data_key="instanceId")
-    InstanceType = fields.Str(required=True, data_key="instanceType")
-    LaunchTime = fields.Str(required=True, data_key="launchDate")
+    InstanceId = fields.Str(required=True, data_key='instanceId')
+    InstanceType = fields.Str(required=True, data_key='instanceType')
+    LaunchTime = fields.Str(required=True, data_key='launchDate')
 
     class Meta:
         ordered = True
 
 
 class MachineIdSchema(Schema):
-    InstanceId = fields.Str(required=True, data_key="instanceId")
+    InstanceId = fields.Str(required=True, data_key='instanceId')
 
     @post_load
     def instance_id(self, data, **kwargs):
